5a35c870c0763d4bfe8840afb77734b9b30003f0
Maxim <maksukh@inbox.ru>
2021-12-24 00:40:49 +0300

    Initialize Spring

---
+++ src/main/kotlin/com/chimber/debtislav/DebtislavApplication.kt
@@ -0,0 +1,11 @@
+package com.chimber.debtislav
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DebtislavApplication
+
+fun main(args: Array<String>) {
+    runApplication<DebtislavApplication>(*args)
+}
---
+++ src/test/kotlin/com/chimber/debtislav/DebtislavApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.chimber.debtislav
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DebtislavApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
