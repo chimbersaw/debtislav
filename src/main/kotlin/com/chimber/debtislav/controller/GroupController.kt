5896f095ba30bd39889dc0e95919a70ba54b31bf
Maxim <maksukh@inbox.ru>
2021-12-27 21:44:10 +0300

    Implement minimizing debts

--- src/main/kotlin/com/chimber/debtislav/controller/GroupController.kt
+++ src/main/kotlin/com/chimber/debtislav/controller/GroupController.kt
@@ -5,6 +5,7 @@ import com.chimber.debtislav.dto.AddUserToGroupRequest
 import com.chimber.debtislav.dto.GroupCreateRequest
 import com.chimber.debtislav.dto.GroupIdRequest
 import com.chimber.debtislav.model.Debt
+import com.chimber.debtislav.repository.GroupRepository
 import com.chimber.debtislav.repository.UserRepository
 import com.chimber.debtislav.service.GroupService
 import org.springframework.web.bind.annotation.*
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/group")
 class GroupController(
     private val userRepository: UserRepository,
+    private val groupRepository: GroupRepository,
     private val groupService: GroupService
 ) : AbstractUserController(userRepository) {
     @PostMapping("/create")
@@ -23,16 +25,23 @@ class GroupController(
 
     @PostMapping("/add")
     fun addUser(@RequestBody request: AddUserToGroupRequest) {
-        groupService.addToGroup(currentUser, userRepository.getUserOrThrow(request.username), request.groupId)
+        val group = groupRepository.getGroupOrThrow(request.groupId)
+        val user = userRepository.getUserOrThrow(request.username)
+        groupService.addToGroup(currentUser, user, group)
     }
 
     @GetMapping("/users")
     fun getAllUsers(@RequestBody request: GroupIdRequest): List<String> {
-        return groupService.getAllUsers(request.id)
+        return groupService.getAllUsers(groupRepository.getGroupOrThrow(request.id))
     }
 
     @GetMapping("/debts")
     fun getAllDebts(@RequestBody request: GroupIdRequest): List<Debt> {
-        return groupService.getAllDebts(request.id)
+        return groupService.getAllDebts(groupRepository.getGroupOrThrow(request.id))
+    }
+
+    @PostMapping("/minimize")
+    fun minimizeDebts(@RequestBody request: GroupIdRequest) {
+        groupService.minimizeDebts(groupRepository.getGroupOrThrow(request.id))
     }
 }
--- src/main/kotlin/com/chimber/debtislav/service/GroupService.kt
+++ src/main/kotlin/com/chimber/debtislav/service/GroupService.kt
@@ -2,18 +2,20 @@ package com.chimber.debtislav.service
 
 import com.chimber.debtislav.exception.NotAuthorizedException
 import com.chimber.debtislav.model.Debt
+import com.chimber.debtislav.model.DebtStatus
 import com.chimber.debtislav.model.Group
 import com.chimber.debtislav.model.User
+import com.chimber.debtislav.repository.DebtRepository
 import com.chimber.debtislav.repository.GroupRepository
 import org.springframework.stereotype.Service
 
 
 @Service
 class GroupService(
-    private val groupRepository: GroupRepository
+    private val groupRepository: GroupRepository,
+    private val debtRepository: DebtRepository
 ) {
-    fun addToGroup(currentUser: User, userToAdd: User, groupId: Long) {
-        val group = groupRepository.getGroupOrThrow(groupId)
+    fun addToGroup(currentUser: User, userToAdd: User, group: Group) {
         if (currentUser.id != group.admin_id) throw NotAuthorizedException()
 
         group.userList.add(userToAdd)
@@ -30,13 +32,70 @@ class GroupService(
         return group.id
     }
 
-    fun getAllUsers(groupId: Long): List<String> {
-        val group = groupRepository.getGroupOrThrow(groupId)
+    fun getAllUsers(group: Group): List<String> {
         return group.userList.map { it.toString() }.toList()
     }
 
-    fun getAllDebts(groupId: Long): List<Debt> {
-        val group = groupRepository.getGroupOrThrow(groupId)
+    fun getAllDebts(group: Group): List<Debt> {
         return group.debtList.toList()
+    }
+
+    fun minimizeDebts(group: Group) {
+        val debts = group.debtList.filter { it.status == DebtStatus.ACTIVE }
+        val balance = mutableMapOf<Long, Int>()
+        for (debt in debts) {
+            balance.compute(debt.loaner_id) { _, v -> (v ?: 0) - debt.amount }
+            balance.compute(debt.lender_id) { _, v -> (v ?: 0) + debt.amount }
+        }
+        val users = balance.keys.toList().sortedBy { balance[it] }
+        println(balance)
+        println(users)
+        var i = 0
+        var j = users.size - 1
+        val newDebts = mutableListOf<Debt>()
+        while (i < j) {
+            val loaner = users[i]
+            val lender = users[j]
+
+            if (balance[loaner] == 0) {
+                i++
+                continue
+            } else if (balance[lender] == 0) {
+                j--
+                continue
+            }
+
+            val loanerAmount = balance[loaner] ?: throw AssertionError()
+            val lenderAmount = balance[lender] ?: throw AssertionError()
+
+            assert(loanerAmount < 0)
+            assert(lenderAmount > 0)
+
+            val amount = minOf(-loanerAmount, lenderAmount)
+            if (amount == -loanerAmount) {
+                i++
+            }
+            if (amount == lenderAmount) {
+                j--
+            }
+
+            val debt = Debt(
+                group_id = group.id,
+                amount = amount,
+                lender_id = lender,
+                loaner_id = loaner,
+                description = "Artificial debt after minimization",
+                status = DebtStatus.ACTIVE
+            )
+            newDebts.add(debt)
+            debtRepository.save(debt)
+        }
+
+        for (debt in debts) {
+            group.debtList.removeIf { it.id == debt.id }
+            debtRepository.delete(debt)
+        }
+        group.debtList.addAll(newDebts)
+        groupRepository.save(group)
     }
 }
